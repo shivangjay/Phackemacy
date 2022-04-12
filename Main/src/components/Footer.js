b05ac4f58fbd02c0863148931ab21c4209be105b
shivangjay <71962325+shivangjay@users.noreply.github.com>
2022-04-12 22:59:02 +0530

    Add files via upload

---
+++ Main/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+export default function Footer() {
+    return (
+      <div className="footer">
+        <section className="footer-links content-container">
+          <div className="text-center p-4">
+            <a className="text-reset fw-bold" href="https://github.com/AkhileshThite/Phackemacy/">GitHub</a>
+          </div>
+        </section>
+      </div>
+    );
+}
